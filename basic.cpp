1b3c9fefd54cd16c68f8e59af0516a8946f2fabf
Ankit Singh <ankitoct03@gmail.com>
2025-09-01 20:32:57 +0530

    cpp

---
+++ basic.cpp
@@ -0,0 +1,9 @@
+// basic cpp program
+#include <iostream>
+
+int main() {
+    std :: cout << "Hello";
+
+    using namespace std;
+    cout << "hello";
+}
---
+++ operations.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+ int main(){
+    using namespace std;
+    int a, b, result;
+    a = 5;
+    b = 8;
+    result = a + b;
+    cout << result;
+    return 0;
+ }
