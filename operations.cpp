f386324a8f17c820985850a179d4b652cc013e39
Ankit Singh <ankitoct03@gmail.com>
2025-09-01 21:25:56 +0530

    include the entire standard library

--- operations.cpp
+++ operations.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
- int main(){
-    using namespace std;
-    int a, b, result;
-    a = 5;
-    b = 8;
-    result = a + b;
-    cout << result;
-    return 0;
- }
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int a, b, result;
+   a = 5;
+   b = 8;
+   result = a + b;
+   cout << result;
+   return 0;
+}
