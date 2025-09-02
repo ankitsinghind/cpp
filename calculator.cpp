43e1f92785de3378efb656093d01bf68b547388c
Ankit Singh <ankitoct03@gmail.com>
2025-09-02 19:21:22 +0530

    basic calculating

---
+++ calculator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cout << "Type a number: ";
+    cin >> x;
+    cout << "Type another number: ";
+    cin >> y;
+
+    int sum = x + y;
+    int minus = x - y;
+    int prod = x * y;
+    int diff = x / y;
+
+    cout << "Sum is: " << sum << "\n";
+    cout << "Minus is: " << minus << "\n";
+    cout << "Prod is: " << prod << "\n";
+    cout << "Diff is: " << diff << "\n";
+    return 0;
+}
