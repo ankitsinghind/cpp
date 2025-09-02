11e72bf0cd2ea7981f8465bb22c3839498f283b1
Ankit Singh <ankitoct03@gmail.com>
2025-09-02 18:48:31 +0530

    student data

---
+++ student_data.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int id = 2;
+    int age = 15;
+    float marks = 99.5;
+    char grade = 'A';
+
+    cout << "student id: " << id << "\n";
+    cout << "Age: " << age << "\n";
+    cout << "Marks: " << marks << "\n";
+    cout << "Grade: " << grade << "\n";
+    return 0;
+}
